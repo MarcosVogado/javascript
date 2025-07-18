7290c773da393c4f963abde4ee149f855f1c862b
Marcos Vogado <marcoshcvogado@gmail.com>
2025-07-18 13:21:13 -0300

    Add dynamic greeting and image based on time of day
    Introduced a carregar() function that updates the greeting message, image, and background color according to the current hour. The HTML now calls carregar() on page load, and minor improvements were made to image alt text and CSS for better layout and accessibility.

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,25 @@
-alert ("olá")
+function carregar() {
+    //Pegando os elementos onde serão inseridos as informações.
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+
+    //Pegando todas as informações do dia, e pondo na variável hora só a hora do dia.
+    var data = new Date()
+    var hora = data.getHours()
+
+    //Fazendo a condição para cada imagem.
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora < 12) {
+        //Bom dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#C1C7CA'
+    } else if (hora < 18) {
+        //Boa tarde!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#81513D'
+    } else {
+        //Boa noite!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#01264B'
+    }
+}
