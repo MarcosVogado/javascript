33f8b6a4b2973fcf6485c97420c3348cdebbfee6
Marcos Vogado <marcoshcvogado@gmail.com>
2025-07-21 15:48:02 -0300

    Add age and gender detection with photo display
    Enhanced the script to detect user's age and gender, displaying a corresponding photo. Fixed label association in HTML and improved result styling and image presentation in CSS.

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -5,9 +5,52 @@ function verificar() {
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
 
-    if (fnao.value.length == 0 || fano.value > ano) {
+    if (fano.value.length < 4 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
-    } else if (){
+    } else {
+        var fsex = document.getElementsByName('radsex')
+
+        var idade = ano - Number(fano.value)
+
+        var gen = ''
+
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gen = 'Homem'
+            if (idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-m.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-m.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-m.jpg')
+            }
+        } else {
+            gen = 'Mulher'
+            if (idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-f.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-f.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-f.jpg')
+            }
+        }
+
+        res.innerHTML = `Detectamos ${gen} com ${idade} anos.`
+        res.appendChild(img)
         
     }
 }
