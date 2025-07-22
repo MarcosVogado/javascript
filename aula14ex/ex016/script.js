99e0edd024e3ace568876808329007e34780e86e
Marcos Vogado <marcoshcvogado@gmail.com>
2025-07-22 16:24:12 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -7,37 +7,37 @@ function contar() {
     var res = document.getElementById('res')
     //Criando o contador
     var i = Number(ini.value)
-    // Limpando o campo
-    res.innerHTML = `Contando: <br>`
     // Validando e contando
-    if (ini.value < 0 || fim.value < 0 || ini.value == fim.value){
+    if (ini.value.length == 0 || fim.value.length == 0 || ini.value == fim.value){
         window.alert('[ERRO] Preencha os campos corretamente')
     } else if (Number(pas.value < 1)){
         window.alert('Já que não foi posto um valor válido, faremos de 1 em 1 ok!')
+        // Limpando o campo
+        res.innerHTML = `Contando: <br>`
+
         if (ini.value < Number(fim.value)) {
             for (i;i<=fim.value;i++){
-                res.innerHTML += ` ${i} `
-            }
-            res.innerText += ' FIM! '
-            
+                res.innerHTML += ` ${i}	\u{27A1} `
+            }   
         } else {
             for (i;i>=fim.value;i--){
-                res.innerHTML += ` ${i} `
+                res.innerHTML += ` ${i}	\u{27A1} `
             }
-            res.innerText += ' FIM! '
         }
+        res.innerHTML += ' FIM! \u{2705}'
     } else {
+        // Limpando o campo
+        res.innerHTML = `Contando: <br>`
+
         if (ini.value < Number(fim.value)) {
             for (i;i<=fim.value;i+=Number(pas.value)){
-                res.innerHTML += ` ${i} `
-            }
-            res.innerText += ' FIM! '
+                res.innerHTML += ` ${i}	\u{27A1} `
+            }   
         } else {
             for (i;i>=fim.value;i-=Number(pas.value)){
-                res.innerHTML += ` ${i} `
+                res.innerHTML += ` ${i}	\u{27A1} `
             }
-            res.innerText += ' FIM! '
         }
-    
+        res.innerHTML += ' FIM! \u{2705}'
     }
 }
