c8d01d5b70b5f57dd52f493cd4b3ec6ad1dcacfd
Marcos Vogado <marcoshcvogado@gmail.com>
2025-07-22 17:32:37 -0300

    Implement tabuada generator UI and logic
    Updated modelo.html to provide a number input and tabuada display. Added tabuada() function in script.js to generate multiplication table for the input number. Improved style.css for better layout and spacing.

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,21 @@
-alert ("olá")
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    tab.innerHTML = ''
+
+    if (num.value.length == 0) {
+        window.alert('[ERRO] Digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+}
